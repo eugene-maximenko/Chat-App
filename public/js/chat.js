dd08c55f848dc20e1097587791e7cf423a0ab473
tiger3120 <zhek4770@gmail.com>
2021-06-20 16:11:04 +0300

    Added a geolocation feature

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,6 +8,8 @@ socket.on('sendMessag', msg => {
     console.log(msg);
 })
 
+
+
 document
     .querySelector('#message-form')
     .addEventListener('submit', (e) => {
@@ -18,3 +20,19 @@ document
         socket.emit('sendMessage', message);
     })
 
+document
+    .querySelector("#send-location")
+    .addEventListener('click', () => {
+        if (!navigator.geolocation) {
+            return alert('Geolocation is not supported by your browser.');
+        }
+
+        navigator.geolocation.getCurrentPosition((position) => {
+            socket.emit('sendLocation', {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            });
+        })
+
+
+    })
