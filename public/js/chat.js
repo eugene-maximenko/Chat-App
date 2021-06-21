1643b17dfb27e6719b3eac099730ba3ac87570da
tiger3120 <zhek4770@gmail.com>
2021-06-21 23:31:20 +0300

    Added a dynamic messages on web page

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,11 +8,14 @@ const $sendLocationButton = document.querySelector('#send-location');
 const $messages = document.querySelector('#messages');
 
 // Templates
-const messageTemplate = document.querySelector('#message-template')
+const messageTemplate = document.querySelector('#message-template').innerHTML;
 
 socket.on('message', (message) => {
     console.log(message);
-
+    const html = Mustache.render(messageTemplate, {
+        message
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
 })
 
 $messageForm
