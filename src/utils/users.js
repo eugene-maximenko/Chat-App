121524afd691eb11e374991c73bd8994cdec67d0
tiger3120 <zhek4770@gmail.com>
2021-06-26 16:48:05 +0300

    Checking the adding user

--- src/utils/users.js
+++ src/utils/users.js
@@ -3,4 +3,13 @@ const users = [];
 // Adding a user to the users array
 const addUser = ({ id, username, room }) => {
     // Clean the data
+    username = username.trim().toLowerCase();
+    room = room.trin().toLowerCase();
+
+    // Validate the data
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
 }
