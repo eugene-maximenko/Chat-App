bb6b0ffde99f72608b9e1156d12813345e26fa7b
tiger3120 <zhek4770@gmail.com>
2021-06-27 18:52:42 +0300

    Send message only to the user`s room

--- src/index.js
+++ src/index.js
@@ -34,18 +34,20 @@ io.on('connection', (socket) => {
     })
 
     socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
         const filter = new Filter();
 
         if (filter.isProfane(message)) {
             return callback('Profanity is not allowed!');
         }
 
-        io.to('Nickolaev').emit('message', generateMessage(message));
+        io.to(user.room).emit('message', generateMessage(message));
         callback();
     })
 
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        const user = getUser(socket.id);
+        io.to(user.room).emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback();
     })
 
