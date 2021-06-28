8aacbdcadc5b57dd9b617cace3d30a73977f9877
tiger3120 <zhek4770@gmail.com>
2021-06-28 23:41:49 +0300

    Created a space in html for rooms template

---
+++ src/utils/rooms.js
@@ -0,0 +1,9 @@
+/* 
+
+Get the actual rooms list in my select element
+
+1. Client emit the event to server when it contains.
+2. The server listens to the event and checks, if there any users in users array, it sends to the client the array with all room names;
+3. If there no users, the server sends nothing.
+
+*/
