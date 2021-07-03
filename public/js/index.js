412b404a58b2f9207a790e6d4c202fb4ca9fbbdb
tiger3120 <zhek4770@gmail.com>
2021-07-03 14:08:07 +0300

    Commented out the file

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,10 @@
+// Setting up a coonection with th server
 const socket = io();
 
+// Sending an event to the server in order to log out the new connection and get the event with the array of active rooms
 socket.emit('joinPageGreeting');
 
-// Templates
+// List of rooms template 
 const listOfRoomsTemplate = document.querySelector('#rooms-list-template').innerHTML;
 
 // Displaying active rooms
@@ -16,25 +18,30 @@ socket.on('roomList', rooms => {
 window.onload = () => {
     let joinButton = document.querySelector('#join-button');
 
+    // Add a click event listener on join button
     joinButton.addEventListener('click', (e) => {
         e.preventDefault();
         console.log('Join button was clicked!');
 
+        // Get the values of username, room and selected elements
         const username = document.querySelector('#username').value;
         let room = document.querySelector('#room').value;
         const selected = document.querySelector('#rooms').value;
 
+        // Restrict user to fill room field and choose an option from select element at the same time 
         if (room && selected !== "Choose your room") {
             return alert(`Please choose the room or only type it's name!`);
         }
 
+        // Assign the value of room variable either from selected element or room element
         if (selected !== "Choose your room") {
             room = selected;
+        } else {
+            room = room;
+            console.log(room);
         }
 
-        room = room;
-        console.log(room);
-
+        // Generate a link for redirecting
         window.location.href = `http://localhost:3000/chat.html?username=${username}&room=${room}`
 
     });
